f6be0d9d2624c5620dccc45aa800d62d84cd4efb
Andrea Pepe <test@test.com>
2020-06-28 14:18:42 +0200

    Aggiunta API di prova

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Restourant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/ristoranti', function () {
+    $risto = Restourant::all()->toJson();
+    return $risto;
 });
